3d19cf256c430f505880c63fdb7077be06291a4b
elihimas <arthurelihimas@gmail.com>
2019-08-29 08:59:29 -0300

    Implemented splash screen

--- app/src/main/java/com/elihimas/shuffler/activities/ShufflerActivity.kt
+++ app/src/main/java/com/elihimas/shuffler/activities/ShufflerActivity.kt
@@ -1,10 +1,22 @@
 package com.elihimas.shuffler.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.elihimas.shuffler.R
 
 class ShufflerActivity : AppCompatActivity() {
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, ShufflerActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            }
+            context.startActivity(intent )
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/elihimas/shuffler/activities/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.elihimas.shuffler.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.elihimas.shuffler.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private companion object {
+        const val REDIRECT_DELAY = 1000L
+    }
+
+    private val handler = Handler()
+    private val redirectCallback = Runnable {
+        ShufflerActivity.start(this)
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler.postDelayed(redirectCallback, REDIRECT_DELAY)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        handler.removeCallbacks(redirectCallback)
+    }
+}
