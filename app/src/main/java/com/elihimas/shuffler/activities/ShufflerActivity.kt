2b4f75338b5eabb15bbc71e1a5fec40a0943e4e7
elihimas <arthurelihimas@gmail.com>
2019-08-29 08:38:37 -0300

    Initial commit

---
+++ app/src/main/java/com/elihimas/shuffler/activities/ShufflerActivity.kt
@@ -0,0 +1,13 @@
+package com.elihimas.shuffler.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elihimas.shuffler.R
+
+class ShufflerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shuffler)
+    }
+}
